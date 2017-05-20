9703d5757da6c147008bf33e9ad9d462e2496794
onkelangil <anders_lykkegaard@hotmail.com>
2017-05-20 13:05:00 +0200

    Unused imports deleted

--- app/src/main/java/com/example/anders/hapticgass/MainActivity.java
+++ app/src/main/java/com/example/anders/hapticgass/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.anders.hapticgass;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
